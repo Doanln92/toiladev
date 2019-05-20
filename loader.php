22d7119ac1b093376696a5f8bd63f56e97750dd7
Doanln92 <30562810+Doanln92@users.noreply.github.com>
2019-05-20 23:11:28 +0700

    update lasnguage

--- loader.php
+++ loader.php
@@ -1,5 +1,12 @@
 <?php
 
+session_start();
+
+
 define('BASE_PATH', dirname(__FILE__));
 define('CORE_PATH', BASE_PATH.'/core');
 include_once(CORE_PATH.'/Arr.php');
+include_once(CORE_PATH.'/Lang.php');
+
+include_once(BASE_PATH.'/helpers/utils.php');
+
--- public/index.php
+++ public/index.php
@@ -2,31 +2,48 @@
 include('../loader.php');
 use Core\Arr;
 
+checkLang();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hello world</title>
 </head>
+
 <body>
     <h2>Tôi là DEV</h2>
-    <pre>
     <?php
-        $person = new Arr([
-            'name' => 'Doãn',
-            'age' => 27,
-        ]);
-
-        // echo $_GET['search']??'chưa gán giá trị';
-        // $person->set('gender', 'female');
-        // $person->ujtgfhtdhdt = "11";
-        // echo $person->count() . '<br>';
-        // echo count($person);
-        echo $person->gender("male");
+    $person = new Arr([
+        'name' => 'Doãn',
+        'age' => 27,
+        'gender' => 'male'
+    ]);
     ?>
-    </pre>
+
+    <p>
+        <?php _text('name');
+        echo ': ' . $person->name; ?>
+    </p>
+    <p>
+        <?php _text('gender');
+        echo ': ' . $person->gender; ?>
+    </p>
+    <p>
+        <?php _text('age');
+        echo ': ' . $person->age; ?>
+    </p>
+
+    <p>
+        <?php
+        $lang = getLocale();
+        ?>
+        <a href="?lang=<?php echo $lang == 'vi' ? 'en' : 'vi' ?>"><?php echo $lang == 'vi' ? 'watvh English Version' : 'Tiếng Việt' ?></a>
+    </p>
 </body>
+
 </html>
